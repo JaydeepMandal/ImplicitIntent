2ac3a76c2106e2288cc2c0644737525363909d16
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-08-05 18:21:44 +0500

    added location opener and text sharing

--- app/src/main/java/com/example/ee/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/ee/implicitintent/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ee.implicitintent;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private EditText webPage;
+    private EditText locEditText;
+    private  EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webPage = (EditText) findViewById(R.id.webEditText);
+        locEditText = (EditText) findViewById(R.id.locEditText);
+        text = (EditText) findViewById(R.id.editText);
     }
 
     public void openWebsite(View v){
         String url = webPage.getText().toString();
+        if(url.equals("")){
+            url = String.valueOf(webPage.getHint());
+        }
         Uri uri = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW,uri);
         if(intent.resolveActivity(getPackageManager())!=null){
@@ -31,4 +39,41 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "No app to handle", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void openLocation(View v){
+
+        String location = locEditText.getText().toString();
+        if(location.equals("")){
+            location = String.valueOf(locEditText.getHint());
+        }
+
+        Uri uri = Uri.parse("geo:0,0?q="+location);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+
+            startActivity(intent);
+
+        }
+        else {
+
+            Toast.makeText(getApplicationContext(), "No app to handle", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void shareText(View v){
+
+        String txt = String.valueOf(text.getText());
+        String mimeType = "text/plain";
+
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with:")
+                .setText(txt)
+                .startChooser();
+    }
+
 }
