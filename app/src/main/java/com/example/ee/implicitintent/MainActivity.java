4b9d059cddb13f741344d58d85b72fe5285165ce
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-08-05 14:40:32 +0500

    added webpage opener

--- app/src/main/java/com/example/ee/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/ee/implicitintent/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.ee.implicitintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText webPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webPage = (EditText) findViewById(R.id.webEditText);
+    }
+
+    public void openWebsite(View v){
+        String url = webPage.getText().toString();
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "No app to handle", Toast.LENGTH_SHORT).show();
+        }
     }
 }
