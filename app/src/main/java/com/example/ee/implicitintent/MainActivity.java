cf91482d17da0b34d1d2822563f1431c8904f46f
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-08-05 23:11:32 +0500

    permission request pop up support for android marshmallow and above

--- app/src/main/java/com/example/ee/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/ee/implicitintent/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ee.implicitintent;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +14,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_CALL_PHONE = 2;
 
     private EditText webPage;
     private EditText locEditText;
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         phoneEditText = (EditText) findViewById(R.id.pnoneEditText);
     }
 
-    public void openWebsite(View v){
+    public void openWebsite(View v){ //method to open website
         String url = webPage.getText().toString();
         if(url.equals("")){
             url = String.valueOf(webPage.getHint());
@@ -42,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void openLocation(View v){
+    public void openLocation(View v){ //method to open location
 
         String location = locEditText.getText().toString();
         if(location.equals("")){
@@ -65,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void shareText(View v){
+    public void shareText(View v){ //method to share text
 
         String txt = String.valueOf(text.getText());
         String mimeType = "text/plain";
@@ -78,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 .startChooser();
     }
 
-    public void openDialer(View v){
+    public void openDialer(View v){ //method to open dialer app
 
         String phoneNo = phoneEditText.getText().toString();
         if(phoneNo.startsWith("+")||phoneNo.startsWith("0")){
@@ -99,7 +105,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void openCallHead(View v){
+    public void openCallHead(View v){ //call to callPhone() method on button click
+
+        //code to handle permission given in manifest specially for android M and above
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   //to check build version in higher than android Marshmallow
+
+            //permission is already available
+            if(checkSelfPermission(Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED){
+                callPhone();
+            }
+
+            //permission is not available
+            else{
+                //user didn't gave permission during first popup of request permission
+                if(shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)){
+                    Toast.makeText(getApplicationContext(), "Required to directly call", Toast.LENGTH_SHORT).show();
+                }
+                //request for CELL_PHONE permission
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL_PHONE);
+            }
+
+        }
+
+    }
+
+    //permission handler method
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode==REQUEST_CALL_PHONE){
+            //request had been granted
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                callPhone();
+            }
+        }
+    }
+
+    public void callPhone(){ //method to directly call phone no.
 
         String phoneNo = phoneEditText.getText().toString();
         if(phoneNo.startsWith("+")||phoneNo.startsWith("0")){
